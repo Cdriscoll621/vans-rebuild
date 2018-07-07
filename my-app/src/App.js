0817dbe1aed424177f89dc33341c74e51a212a30
Charlotte Driscoll <charlotte.d.driscoll@icloud.com>
2018-07-07 13:00:04 -0500

    first

---
+++ my-app/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+import Nav from './Components/Nav/Nav';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav>
+        <img src='./Components/Images/vans-logo.svg' alt='logo'/>
+          <h1 className="App-title">Vans Rebuild</h1>
+        </Nav>
+        <p className="App-intro">
+        My React App
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
